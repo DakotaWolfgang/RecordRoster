d9c20647c13e3f904698cdc24d511b9293dcdbcf
Jacob Noah <jacobnoahbusiness@gmail.com>
2025-03-25 15:56:07 -0400

    Clean layouts & complete add functionality/details view

---
+++ RecordRoster/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.EnterpriseServices;
+using System.Web.Mvc;
+using System.Web;
+using RecordRoster.Models;
+using RecordRoster.DataAccessLayer;
+using System.Linq;
+using System;
+
+namespace RecordRoster.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly RecordRosterDb _context = new RecordRosterDb();
+
+        public ActionResult Index()
+        {
+            var albums = _context.Albums.ToList();
+            ViewBag.RandomAlbum = albums.OrderBy(a => Guid.NewGuid()).FirstOrDefault();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
